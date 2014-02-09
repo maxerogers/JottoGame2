17d80dcd3df80e007221b99e9e1b234f0500d56e
Max Rogers <maxrogers90@gmail.com>
2014-02-08 23:47:09 -0500

    fixed some bugs and now color buttons work

--- gen/com/example/jottogame2/R.java
+++ gen/com/example/jottogame2/R.java
@@ -24,12 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int AFLayout=0x7f080004;
         public static final int EditText01=0x7f080000;
+        public static final int GLLayout=0x7f080005;
         public static final int Giveup=0x7f080002;
         public static final int Guess=0x7f080003;
-        public static final int GuessList=0x7f080004;
+        public static final int GuessList=0x7f080009;
         public static final int Guesslayout=0x7f080001;
-        public static final int action_settings=0x7f080005;
+        public static final int MRLayout=0x7f080006;
+        public static final int SXLayout=0x7f080007;
+        public static final int YZLayout=0x7f080008;
+        public static final int action_settings=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/jottogame2/MainActivity.java
+++ src/com/example/jottogame2/MainActivity.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.database.SQLException;
 import android.graphics.Color;
 import android.text.Spannable;
@@ -14,8 +16,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,23 +28,94 @@ public class MainActivity extends Activity {
 	private String TAG = "MAIN";
 	private String answer;
 	private int[] abc;
+	private int defaultButtonColor;
 	private ArrayList<String> guesses = new ArrayList<String>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		loadData();
-		newGame();
 		newGameListener();
 		abc = new int[26];
-		for(int i=0;i<abc.length;i++){	abc[i] = 2; }
+		for(int i=0;i<abc.length;i++){	abc[i] = 1; }
+		addAZButtons();
+		newGame();
 		addGuessListener();
 	}
-	
+
+	private void addAZButtons() {
+		LinearLayout llaf = (LinearLayout) findViewById(R.id.AFLayout);
+		OnClickListener buttonListener = new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Button btn = (Button) v;
+				char c = btn.getText().toString().charAt(0);
+				Log.i(TAG,""+c);
+				int x = c - 'A';
+				if(abc[x] > 1){
+					btn.setBackgroundResource(android.R.drawable.btn_default);
+					abc[x] = 0;
+				}else if(abc[x] < 1){
+					btn.setBackgroundColor(Color.RED);
+					abc[x] = 1;
+				}else{
+					btn.setBackgroundColor(Color.GREEN);
+					abc[x] = 2;
+				}
+				recolorLetter();
+			}
+		};
+		for(char c= 'A'; c <= 'F'; c++)
+		{
+			Button button = new Button(this, null, android.R.attr.buttonStyleSmall);
+			button.setText(""+c);
+			llaf.addView(button);
+			button.setOnClickListener(buttonListener);
+			button.setId(c);
+		}
+		LinearLayout llgf = (LinearLayout) findViewById(R.id.GLLayout);
+		for(char c= 'G'; c <= 'L'; c++)
+		{
+			Button button = new Button(this, null, android.R.attr.buttonStyleSmall);
+			button.setText(""+c);
+			llgf.addView(button);
+			button.setOnClickListener(buttonListener);
+			button.setId(c);
+		}
+		LinearLayout llmr = (LinearLayout) findViewById(R.id.MRLayout);
+		for(char c= 'M'; c <= 'R'; c++)
+		{
+			Button button = new Button(this, null, android.R.attr.buttonStyleSmall);
+			button.setText(""+c);
+			llmr.addView(button);
+			button.setOnClickListener(buttonListener);
+			button.setId(c);
+		}
+		LinearLayout llsx = (LinearLayout) findViewById(R.id.SXLayout);
+		for(char c= 'S'; c <= 'X'; c++)
+		{
+			Button button = new Button(this, null, android.R.attr.buttonStyleSmall);
+			button.setText(""+c);
+			llsx.addView(button);
+			button.setOnClickListener(buttonListener);
+			button.setId(c);
+		}
+		LinearLayout llyz = (LinearLayout) findViewById(R.id.YZLayout);
+		for(char c= 'Y'; c <= 'Z'; c++)
+		{
+			Button button = new Button(this, null, android.R.attr.buttonStyleSmall);
+			button.setText(""+c);
+			llyz.addView(button);
+			button.setOnClickListener(buttonListener);
+			button.setId(c);
+		}
+	}
+
 	private void addGuessListener() {
 		Button btnG = (Button) findViewById(R.id.Guess);
+		btnG.setBackgroundResource(android.R.drawable.btn_default);
 		btnG.setOnClickListener(new OnClickListener(){
-
+			
 			@Override
 			public void onClick(View v) {
 				EditText et01 = (EditText) findViewById(R.id.EditText01);
@@ -55,6 +130,13 @@ public class MainActivity extends Activity {
 						TextView guessList = (TextView) findViewById(R.id.GuessList);
 						String temp = guessList.getText().toString();
 						guessList.setText(input+": ("+inCommon(answer, input).length()+") \n"+temp);
+						InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+						imm.hideSoftInputFromWindow(et01.getWindowToken(), 0);
+						if(input.equals(answer))
+						{
+							
+						}
+						recolorLetter();
 					}else{
 						//Log.i(TAG, "NOT a valid guess");
 						Toast.makeText(getApplicationContext(), input+" is not a valid guess", Toast.LENGTH_SHORT).show();
@@ -96,6 +178,7 @@ public class MainActivity extends Activity {
 	
 	private void newGameListener(){
 		Button btnNG = (Button) findViewById(R.id.Giveup);
+		btnNG.setBackgroundResource(android.R.drawable.btn_default);
 		btnNG.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View arg0) {
@@ -107,12 +190,17 @@ public class MainActivity extends Activity {
 	private void newGame() {
 		answer = db.getRandomWord();
 		guesses = new ArrayList<String>();
-		Log.v(TAG, answer);
+		Log.i(TAG, answer);
 		TextView guessList = (TextView) findViewById(R.id.GuessList);
 		guessList.setText("");
+		for(char c = 'A';c<='Z';c++)
+		{
+			Button btn = (Button) findViewById(c);
+			btn.setBackgroundResource(android.R.drawable.btn_default);
+		}
 	}
 	
-	private void recolorLetter(int index) {
+	private void recolorLetter() {
 		TextView guessList = (TextView) findViewById(R.id.GuessList);
 		String temp = guessList.getText().toString();
 		
